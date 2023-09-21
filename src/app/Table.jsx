275e359cff83fae5b7bb1d422f81a8ea937fe57c
Stanislav Beliaev <testerbeliaev@gmail.com>
2023-09-21 17:30:01 +0300

    buttons get their functionality

--- src/app/Table.jsx
+++ src/app/Table.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-export const Table = ({data, ...restProps}) => {
+export const Table = ({data,...restProps}) => {
+    
     return (
+        
         <table class="border-separate border-spacing-0 table-fixed w-full z-0 relative h-1">
             <thead >
                 <tr >
@@ -10,7 +12,20 @@ export const Table = ({data, ...restProps}) => {
                     <th class="border">Standart</th>
                     <th class="border">Basic</th>
                 </tr>
+                <tr>
+                <td></td>
+                    {Object.entries(data).map(([id,val],idx) => {
+                        return(
+                            
+                            
+                            <td class="border">{val.currency} {val.basePrice}</td>
+                            
+                        )
+                    })}
+                </tr>
             </thead>
         </table>
+        
     );
+    
 }
--- src/app/page.js
+++ src/app/page.js
@@ -9,13 +9,36 @@ export default function Home() {
   const [button1, setButton1] = useState(true);
   const [button2, setButton2] = useState(false);
   const [button3, setButton3] = useState(false);
+  const [monthlyPrice, setmonthlyPrice] = useState([]);
+  const [yearlyPrice, setyearlyPrice] = useState([]);
+  const [yearly2Price, setyearly2Price] = useState([]);
+
   const [years2, setYears2] = useState([]);
 
-
   useEffect(() => {
-    setYears2(jsonData.plans[0].prices[0].yearly2)
-    console.log(years2)
-  })
+    // setYears2(Object.entries(jsonData.plans[0].prices[0].yearly2));
+    let monthlyPrices = [];
+    let yearlyPrices = [];
+    let yearly2Prices = [];
+
+    // Перебераем все планы
+    for (let i = 0; i < jsonData.plans.length; i++) {
+      let plan = jsonData.plans[i];
+
+      // Получаем цены для каждого месяца\года
+      let monthlyPrice = plan.prices[0].monthly;
+      let yearlyPrice = plan.prices[0].yearly;
+      let yearly2Price = plan.prices[0].yearly2;
+
+      monthlyPrices.push(monthlyPrice);
+      yearlyPrices.push(yearlyPrice);
+      yearly2Prices.push(yearly2Price);
+    }
+    setmonthlyPrice(monthlyPrices);
+    setyearlyPrice(yearlyPrices)
+    setyearly2Price(yearly2Prices)
+  },[]);
+
   const handleClick1 = () => {
     setButton1(true);
     setButton2(false);
@@ -25,6 +48,7 @@ export default function Home() {
     setButton1(false);
     setButton2(true);
     setButton3(false);
+
   };
   const handleClick3 = () => {
     setButton1(false);
@@ -32,6 +56,7 @@ export default function Home() {
     setButton3(true);
   };
 
+console.log(yearly2Price)
   return (
     <main>
       <div class="flex justify-center">
@@ -52,7 +77,7 @@ export default function Home() {
             </Button>
           </div>
           <div class="flex justify-center space-between">
-            <Table data={jsonData}></Table>
+              <Table data={button1 === true ? yearly2Price : button2 === true ? yearlyPrice : button3 === true ? monthlyPrice : yearly2Price}></Table>
           </div>
         </div>
       </div>
